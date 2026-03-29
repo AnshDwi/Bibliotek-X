281c6d85d6fa44dabaf1c49fbbcdac894bb6d96b
Anshika <anshdwi21@gmail.com>
2026-03-29 11:36:38 +0530

    connect frontend to deployed backend

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://bibliotek-backend.onrender.com/api",
   withCredentials: true
 });
 
@@ -14,4 +14,3 @@ api.interceptors.request.use((config) => {
 });
 
 export { api };
-
